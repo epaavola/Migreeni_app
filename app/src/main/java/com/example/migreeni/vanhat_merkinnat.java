b56061bd006ee022c7044818664b6a71c69b5b1d
Katariina <katariina.kaita@gmail.com>
2019-05-02 12:23:56 +0300

    Lisätty: vanhat_merkinnat_avattuinfo, activity/layout Muutettu: vanhat_merkinnat, layout & liittyvää

--- app/src/main/java/com/example/migreeni/vanhat_merkinnat.java
+++ app/src/main/java/com/example/migreeni/vanhat_merkinnat.java
@@ -1,13 +1,21 @@
 package com.example.migreeni;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class vanhat_merkinnat extends AppCompatActivity {
+
+    private static final String tagi = "Merkkaus";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vanhat_merkinnat);
+
+        Log.d(tagi, "aktiviteetissa: vanhat merkinnat");
     }
+
 }
